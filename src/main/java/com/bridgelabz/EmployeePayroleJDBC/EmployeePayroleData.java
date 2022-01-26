64e34087f65532f37524d8e97407e795e85318e0
Nandini Yarra <yarranandhini1416@gmail.com>
2022-01-26 17:06:29 +0530

    ADDDED:connection with database

--- src/main/java/com/bridgelabz/EmployeePayroleJDBC/EmployeePayroleData.java
+++ src/main/java/com/bridgelabz/EmployeePayroleJDBC/EmployeePayroleData.java
@@ -22,13 +22,4 @@ public class EmployeePayroleData {
 		
 		return "EmployeeId: "+employeeId+", EmployeeName: "+employeeName+", EmployeeSalary: "+employeeSalary;
 	}
-	@Override
-	public boolean equals(Object object) {
-		if(this == object)
-			return true;
-		if(object == null || getClass() != object.getClass())
-			return false;
-		EmployeePayroleData that = (EmployeePayroleData) object;
-		return employeeId == that.employeeId && Double.compare(that.employeeSalary,  employeeSalary) == 0 && employeeName.equals(that.employeeName);
-	}
-}
+	
